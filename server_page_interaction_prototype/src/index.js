8ae91482c3d5080faf0d705a2bb34563bda01d9f
Ben <ben.price2013@gmail.com>
2022-04-04 13:57:14 -0500

    Clean up code

--- server_page_interaction_prototype/src/index.js
+++ server_page_interaction_prototype/src/index.js
@@ -1,11 +1,4 @@
-const Http = new XMLHttpRequest();
-
 const body = document.querySelector('body');
-
-function send_request() {
-    Http.open("GET", '/data/data.json');
-    Http.send();
-}
 
 function setCookie(name, value, days) {
     var expires = "";
@@ -41,7 +34,6 @@ const handle_login = (event) => {
         // to the ID of the user in the database internally. This is insanely insecure but will work for our
         // demo.
         if (data.Accepted) {
-            console.log(data);
             setCookie("UserID", data.UserID, 1);
             // Put logic for loading user profile page here
         }
@@ -51,52 +43,22 @@ const handle_login = (event) => {
     })
     return false;
 }
-
-Http.onreadystatechange=function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // const resp = JSON.parse(this.responseText);
-        // for (const obj of resp) {
-        //     const div = document.createElement('div');
-        //     div.setAttribute('class', 'card');
-        //     const h4 = document.createElement('h4');
-        //     const a = document.createElement('a');
-        //     const img = document.createElement('img');
-        //     body.append(div);
-        //     h4.append(a);
-        //     div.append(h4);
-        //     div.append(img);
-        //     a.setAttribute('href', obj.info);
-        //     img.setAttribute('src', obj.pic);
-        //     a.innerHTML = obj.name;
-        // }
-
-        // const xmlhttp = new XMLHttpRequest();
-        // xmlhttp.open("POST", "/requests");
-        // xmlhttp.setRequestHeader("Content-type", "application/json");
-        // const content = JSON.stringify({'Username': 'First User', 'Password': '7654321'});
-
-        var userCounter;
-        const loc = document.cookie.indexOf('User')
-        if (loc  == -1) {
-            userCounter = 1;
-            setCookie('User', userCounter, 1);
-        }
-        else {
-            const restOfString = document.cookie.substring(loc);
-            const endOfCookie = restOfString.indexOf(';');
-            var currentCount;
-            if (endOfCookie == -1) {
-                currentCount = restOfString.substring(5);
-            }
-            else {
-                currentCount = restOfString.substring(5, endOfCookie);
-            }
-            userCounter = parseInt(currentCount) + 1;
-            setCookie('User', userCounter, 1);
-        }
-
-        // xmlhttp.send(content);
-    }
-}
-
-send_request();
+//         var userCounter;
+//         const loc = document.cookie.indexOf('User')
+//         if (loc  == -1) {
+//             userCounter = 1;
+//             setCookie('User', userCounter, 1);
+//         }
+//         else {
+//             const restOfString = document.cookie.substring(loc);
+//             const endOfCookie = restOfString.indexOf(';');
+//             var currentCount;
+//             if (endOfCookie == -1) {
+//                 currentCount = restOfString.substring(5);
+//             }
+//             else {
+//                 currentCount = restOfString.substring(5, endOfCookie);
+//             }
+//             userCounter = parseInt(currentCount) + 1;
+//             setCookie('User', userCounter, 1);
+//         }
