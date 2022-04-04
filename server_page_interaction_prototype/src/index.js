1042983547752515381a77b648358259696eb4e7
Ben <ben.price2013@gmail.com>
2022-04-04 09:36:37 -0500

    Comment out prototype lines, test cookie logic

--- server_page_interaction_prototype/src/index.js
+++ server_page_interaction_prototype/src/index.js
@@ -20,40 +20,49 @@ function setCookie(name, value, days) {
 
 Http.onreadystatechange=function() {
     if (this.readyState == 4 && this.status == 200) {
-        const resp = JSON.parse(this.responseText);
-        for (const obj of resp) {
-            const div = document.createElement('div');
-            div.setAttribute('class', 'card');
-            const h4 = document.createElement('h4');
-            const a = document.createElement('a');
-            const img = document.createElement('img');
-            body.append(div);
-            h4.append(a);
-            div.append(h4);
-            div.append(img);
-            a.setAttribute('href', obj.info);
-            img.setAttribute('src', obj.pic);
-            a.innerHTML = obj.name;
-        }
+        // const resp = JSON.parse(this.responseText);
+        // for (const obj of resp) {
+        //     const div = document.createElement('div');
+        //     div.setAttribute('class', 'card');
+        //     const h4 = document.createElement('h4');
+        //     const a = document.createElement('a');
+        //     const img = document.createElement('img');
+        //     body.append(div);
+        //     h4.append(a);
+        //     div.append(h4);
+        //     div.append(img);
+        //     a.setAttribute('href', obj.info);
+        //     img.setAttribute('src', obj.pic);
+        //     a.innerHTML = obj.name;
+        // }
 
-        const xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("POST", "/requests");
-        xmlhttp.setRequestHeader("Content-type", "application/json");
-        const content = JSON.stringify({'Username': 'First User', 'Password': '7654321'});
+        // const xmlhttp = new XMLHttpRequest();
+        // xmlhttp.open("POST", "/requests");
+        // xmlhttp.setRequestHeader("Content-type", "application/json");
+        // const content = JSON.stringify({'Username': 'First User', 'Password': '7654321'});
 
         var userCounter;
-        if (document.cookie == '') {
+        const loc = document.cookie.indexOf('User')
+        if (loc  == -1) {
             userCounter = 1;
             setCookie('User', userCounter, 1);
         }
         else {
-            var holder = document.cookie.substring(5);
+            const restOfString = document.cookie.substring(loc);
+            const endOfCookie = restOfString.indexOf(';');
+            var currentCount;
+            if (endOfCookie == -1) {
+                currentCount = restOfString.substring(5);
+            }
+            else {
+                currentCount = restOfString.substring(5, endOfCookie);
+            }
             console.log(document.cookie[5]);
-            userCounter = parseInt(holder) + 1;
+            userCounter = parseInt(currentCount) + 1;
             setCookie('User', userCounter, 1);
         }
 
-        xmlhttp.send(content);
+        // xmlhttp.send(content);
     }
 }
 
